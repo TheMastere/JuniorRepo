94e66c6fa25a1f8d8889116287862bf60f060a89
Master <zoom906@gmail.com>
2019-05-18 18:58:34 +0300

    My first PythonBot

---
+++ alievs_bot.py
@@ -0,0 +1,18 @@
+#Импортируем нужные компоненты
+from telegram.ext import Updater, CommandHandler
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+def greet_user(bot, update):
+    print('Вызван /start')
+def main():
+    mybot = Updater("686615841:AAGc5WcQiaZK-ZB4DfzxLfzC-heBVyaX5_Y", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+
+    mybot.start_polling()
+    mybot.idle()
+
+#Вызываем функцию
+main()
