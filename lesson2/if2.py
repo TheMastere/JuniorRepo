2e8c18aba6df968a80675ac2217e168aa96eb298
Master <zoom906@gmail.com>
2019-05-28 20:01:28 +0300

    Исправлены ошибки в задании

--- lesson2/if2.py
+++ lesson2/if2.py
@@ -1,12 +1,12 @@
-def sravnenie_strok(stroka1, stroka2):
-    if type(stroka1) != str:
-        print("0")
-    if type(stroka2) != str:
-        print("0")
-    if stroka1 == stroka2:
-        print("1")
-    if len(stroka1) > len(stroka2):
-        print("2")
-    if stroka1 != "learn":
-        print("3")
-print(sravnenie_strok("Python", "learn"))
+def comparison_strings(string1, string2):
+    if type(string1) != str:
+        return ("0")
+    if type(string2) != str:
+        return ("0")
+    if string1 == string2:
+        return ("1")
+    if len(string1) > len(string2):
+        return ("2")
+    elif string1 != "learn" or string1 != string2:
+        return ("3")
+print(comparison_strings("learj", "learn"))
